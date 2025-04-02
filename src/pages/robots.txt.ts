1c7124f5a5aebfce06afa07fd99bb60cafaea2e2
onishi taisei <taisei.developer@gmail.com>
2025-04-02 21:56:41 +0900

    🚀 add: Google Search Console

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -6,7 +6,7 @@ export function GET(context: Context) {
   const robots = `
 User-agent: *
 Allow: /
-  
+
 Sitemap: ${new URL('sitemap-index.xml', context.site).href}`.trim()
 
   return new Response(robots, {

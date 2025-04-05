bf8826c269232195bd218b1cd53839e05d63e440
onishi taisei <taisei.developer@gmail.com>
2025-04-05 19:57:24 +0900

    👻 fix: defineConfig修正

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 export default defineConfig({
-  site: 'https://astro-theme-vitesse.netlify.app/',
+  site: 'https://taiseidev.com',
   server: {
     port: 1977,
   },
